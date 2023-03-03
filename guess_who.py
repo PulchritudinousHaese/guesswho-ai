621b4a51a666cd91ecc2867d25b9ab17b94d60b9
Mikhail <92236855+PulchritudinousHaese@users.noreply.github.com>
2023-03-03 18:19:40 -0500

    Update guess_who.py

--- guess_who.py
+++ guess_who.py
@@ -1,1 +1,11 @@
+"""CSC111 Winter 2023 Project: TGuess Who Artifical Intelligence
 
+This module contains the classes and necessary functions to execute
+the backend of the game "Guess Who".
+"""
+
+import random
+import copy
+
+import plotly
+import tkinter as tk

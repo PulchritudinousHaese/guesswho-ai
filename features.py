96170f20a910d3b72e9c82a531d83b3eb2db12c2
anniexwang <126923400+anniexwang@users.noreply.github.com>
2023-03-12 14:38:32 -0400

    Update features.py

--- features.py
+++ features.py
@@ -1,16 +1,5 @@
 """CSC111 Winter 2023 Project: Guess Who Artifical Intelligence
 This module contains the features as CONSTANTS for the game's Person class. 
-
-Instance Attributes:
-  - name: The person's name (for the final guess)
-  - ear_size: The size of the person's ears.
-  - hair_style: The person's hairstyle. 
-  - hair_length: The person's hair length.
-  - hair_colour: The person's hair colour. None if none.
-  - nose_size: The person's nose size.
-  - facial_hair: The person's facial_hair. None if none.
-  - accessory: The person's accesory. None if none.
-  - mouth_size: The person's mouth size.  
 """
 
 # ear_size

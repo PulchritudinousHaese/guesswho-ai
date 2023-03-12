37cfe20e7eac6d099f822e979122f32b566df299
anniexwang <126923400+anniexwang@users.noreply.github.com>
2023-03-12 15:06:58 -0400

    Update artificial_intelligence.py

--- artificial_intelligence.py
+++ artificial_intelligence.py
@@ -1,21 +1,4 @@
-"""CSC111 Winter 2023 Assignment 2: Trees, Wordle, and Artificial Intelligence (Part 3)
-
-Instructions (READ THIS FIRST!)
-===============================
-
-This Python module contains the start of functions and/or classes you'll define
-for Part 3 of this assignment. You should NOT make any changes to a2_adversarial_wordle.py.
-
-Copyright and Usage Information
-===============================
-
-This file is provided solely for the personal and private use of students
-taking CSC111 at the University of Toronto St. George campus. All forms of
-distribution of this code, whether as given or with any changes, are
-expressly prohibited. For more information on copyright for CSC111 materials,
-please consult our Course Syllabus.
-
-This file is Copyright (c) 2023 Mario Badr, David Liu, and Isaac Waller.
+This file is Copyright (c) 2023 Annie Wang, Mikhail Skazhenyuk, Xinyuan Gu, Ximei Lin.
 """
 import random
 from typing import Optional

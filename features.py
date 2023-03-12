458ad481899028f8a3c39bb20917ce3226e4e40e
Mikhail <92236855+PulchritudinousHaese@users.noreply.github.com>
2023-03-12 14:30:34 -0400

    Create features.py

---
+++ features.py
@@ -0,0 +1,55 @@
+"""CSC111 Winter 2023 Project: Guess Who Artifical Intelligence
+This module contains the features as CONSTANTS for the game's Person class. 
+
+Instance Attributes:
+  - name: The person's name (for the final guess)
+  - ear_size: The size of the person's ears.
+  - hair_style: The person's hairstyle. 
+  - hair_length: The person's hair length.
+  - hair_colour: The person's hair colour. None if none.
+  - nose_size: The person's nose size.
+  - facial_hair: The person's facial_hair. None if none.
+  - accessory: The person's accesory. None if none.
+  - mouth_size: The person's mouth size.  
+"""
+
+# ear_size
+BIGEARS = 'E'
+SMALLEARS = 'e'
+
+# hair_style
+STRAIGHT = 'S'
+CURLY = 'C'
+WAVY = 'W'
+
+# hair_length
+LONGHAIR = 'L'
+MEDIUMHAIR = 'M'
+SHORTHAIR = 'T'
+BALD = 'D'
+
+# hair_colour
+BLONDE = 'O'
+BLACK = 'B'
+BROWN = 'b'
+RED = 'R'
+GRAY = 'G'
+
+# nose_size
+BIGNOSE = 'N'
+SMALLNOSE = 'n'
+
+# facial_hair
+BEARD = 'd'
+MOUSTACHE = 'm'
+FULLBEARD = 'D'
+
+# accessory
+HAT = 'H'
+REDCHEEKS = 'r'
+
+# mouth_size
+BIGMOUTH = 'U'
+MEDIUMMOUTH = 'V'
+SMALLMOUTH = 'u'
+

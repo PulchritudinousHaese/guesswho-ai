643841d2c6a3827b3d95c681fd1d23bd69c7d6d2
anniexwang <126923400+anniexwang@users.noreply.github.com>
2023-03-27 18:56:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,6 @@
 
 This module is responsible for combining the backend and frontend of the
 Guess Who application.
+
+This file is Copyright (c) 2023 Annie Wang, Mikhail Skazhenyuk, Xinyuan Gu, Ximei Lin.
 """

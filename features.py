d27237c961b6965364db4df491f025b21030f28c
Mikhail <92236855+PulchritudinousHaese@users.noreply.github.com>
2023-03-29 20:20:58 -0400

    Update features.py

--- features.py
+++ features.py
@@ -1,45 +1,85 @@
 """CSC111 Winter 2023 Project: Guess Who Artifical Intelligence
-This module contains the features as CONSTANTS for the game's Person class. 
-This file is Copyright (c) 2023 Annie Wang, Mikhail Skazhenyuk, Xinyuan Gu, Ximei Lin.
+This module contains the features as CONSTANTS for the game's Person class.
 """
 
-# ear_size
-BIGEARS = 'E'
-SMALLEARS = 'e'
-
-# hair_style
-STRAIGHT = 'S'
-CURLY = 'C'
-WAVY = 'W'
-
-# hair_length
-LONGHAIR = 'L'
-MEDIUMHAIR = 'M'
-SHORTHAIR = 'T'
+HAIRPARTITION = 'A'
+CURLYHAIR = 'B'
+HAT = 'C'
 BALD = 'D'
+SMALLMOUTH = 'E'
+LONGHAIR = 'F'
+RED = 'G'
+GRAY = 'H'
+BROWN = 'I'
+BLONDE = 'J'
+BLACK = 'K'
+BIGMOUTH = 'L'
+BIGNOSE = 'M'
+REDCHEEKS = 'N'
+BLUEEYES = 'O'
+EYEBROWS = 'P'
+MOUSTACHE = 'Q'
+BEARD = 'R'
+GLASSES = 'S'
+EARRING = 'T'
+BIGEARS = 'U'
 
-# hair_colour
-BLONDE = 'O'
-BLACK = 'B'
-BROWN = 'b'
-RED = 'R'
-GRAY = 'G'
 
-# nose_size
-BIGNOSE = 'N'
-SMALLNOSE = 'n'
+# # ear_size
+# BIGEARS = 'E'
+# SMALLEARS = 'e'
+#
+# EAR_SIZES = ['E', 'e']
+#
+# # hair_style
+# STRAIGHT = 'S'
+# CURLY = 'C'
+# WAVY = 'W'
+#
+# HAIR_STYLES = ['S', 'C', 'W']
+#
+# # hair_length
+# LONGHAIR = 'L'
+# MEDIUMHAIR = 'M'
+# SHORTHAIR = 'T'
+# BALD = 'D'
+#
+# HAIR_LENGTHS = ['L', 'M', 'T', 'D']
+#
+# # hair_colour
+# BLONDE = 'O'
+# BLACK = 'B'
+# BROWN = 'b'
+# RED = 'R'
+# GRAY = 'G'
+#
+# HAIR_COLOURS = ['O', 'B', 'b', 'R', 'G']
+#
+# # nose_size
+# BIGNOSE = 'N'
+# SMALLNOSE = 'n'
+#
+# NOSE_SIZES = ['N', 'n']
+#
+# # facial_hair
+# BEARD = 'd'
+# MOUSTACHE = 'm'
+# FULLBEARD = 'D'
+#
+# FACIAL_HAIR = ['d', 'm', 'D']
+#
+# # accessory
+# HAT = 'H'
+# REDCHEEKS = 'r'
+#
+# ACCESSORIES = ['H', 'r']
+#
+# # mouth_size
+# BIGMOUTH = 'U'
+# MEDIUMMOUTH = 'V'
+# SMALLMOUTH = 'u'
+#
+# MOUTH_SIZES = ['U', 'V', 'u']
 
-# facial_hair
-BEARD = 'f'
-MOUSTACHE = 'm'
-FULLBEARD = 'F'
-
-# accessory
-HAT = 'H'
-REDCHEEKS = 'r'
-
-# mouth_size
-BIGMOUTH = 'U'
-MEDIUMMOUTH = 'V'
-SMALLMOUTH = 'u'
-
+# CHARACTERISTICS = [EAR_SIZES, HAIR_STYLES, HAIR_LENGTHS, HAIR_COLOURS, NOSE_SIZES, FACIAL_HAIR, ACCESSORIES,
+#                    MOUTH_SIZES]

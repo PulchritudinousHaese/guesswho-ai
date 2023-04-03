a3b0c224b48a135931453d887d00399304547f75
hixmyjk <126923384+hixmyjk@users.noreply.github.com>
2023-04-02 22:16:40 -0400

    Update game_tree.py

--- game_tree.py
+++ game_tree.py
@@ -145,3 +145,14 @@ if __name__ == '__main__':
     import doctest
 
     doctest.testmod(verbose=True)
+    # When you are ready to check your work with python_ta, uncomment the following lines.
+    # (In PyCharm, select the lines below and press Ctrl/Cmd + / to toggle comments.)
+    # You can use "Run file in Python Console" to run PythonTA,
+    # and then also test your methods manually in the console.
+    # import python_ta
+    # python_ta.check_all(config={
+    #     'max-line-length': 120,
+    #     'max-nested-blocks': 4,
+    #     'extra-imports': ['random', 'a2_adversarial_wordle', 'a2_game_tree'],
+    #     'allowed-io': ['run_learning_algorithm']
+    # })

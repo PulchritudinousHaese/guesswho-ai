eea9188511f2821f94e3e7d6804845a9a4a5a6ab
Mikhail <92236855+PulchritudinousHaese@users.noreply.github.com>
2023-04-02 23:22:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,20 @@ may choose which opponent to play against.
 
 This file also contins the main function to run games and test performaces between AIs.
 """
-import guess_who
-from guess_who import Player, RandomPlayer, GreedyPlayer, PoorPlayer
-import csv
-
 from __future__ import annotations
 
+import csv
 from tkinter import *
 
-import interface as IF
 import guess_who as GW
-import artificial_intelligence as AI
+from guess_who import Player
 
-game_settings = {'question': ''}
+import artificial_intelligence as AI
+from artificial_intelligence import RandomPlayer, GreedyPlayer, PoorPlayer
+
+import interface as IF
+
+GAME_SETTINGS = {'question': ''}
 
 
 ###########################################################
@@ -46,15 +47,15 @@ def run_games(num: int, players: list[Player], num_cha: int, file: csv, plot: bo
     results = {'num_games': [n for n in range(1, num + 1)], pl1_name: default, pl2_name: default1}
     game_sta = {players[0].name: 0, players[1].name: 0}
     for i in range(0, num):
-        can = guess_who.create_candidates(file, num_cha)
-        question = guess_who.generate_all_possible_questions(file)
+        can = GW.create_candidates(file, num_cha)
+        question = GW.generate_all_possible_questions(file)
         player1.candidates = can
         player2.candidates = can.copy()
         player1.questions = question
         player2.questions = question.copy()
         player1.select_spy()
         player2.select_spy()
-        winner = guess_who.run_game([player1, player2], can.copy())
+        winner = AI.run_game([player1, player2], can.copy())
         if winner == player1.name or winner == player2.name:
             game_sta[winner] += 1
             results[winner][i] = 1
@@ -63,7 +64,7 @@ def run_games(num: int, players: list[Player], num_cha: int, file: csv, plot: bo
         pl2_wins = game_sta[pl2_name]
         print(f'[winning_probability:{pl1_name}: {(pl1_wins / num) * 100}%, {pl2_name}: {(pl2_wins / num) * 100}%]')
     if plot:
-        guess_who.plot_game_statistics(results, player1.name, player2.name)
+        AI.plot_game_statistics(results, player1.name, player2.name)
 
     return results
 
@@ -71,44 +72,60 @@ def run_games(num: int, players: list[Player], num_cha: int, file: csv, plot: bo
 class HumanPlayer(GW.Player):
     """The class representing the human player."""
 
-    def __init__(self, candidates: dict[str, dict[str, str]], questions: list[str]):
+    def __init__(self, candidates: dict[str, dict[str, str]], questions: list[str]) -> None:
         GW.Player.__init__(self, candidates, questions, 'HumanPlayer')
 
     def make_guesses(self, game: GW.GuessWho) -> str:
         """Make a guess at the enemy's spy."""
-        return IF.guess_frame_objects['label']['text']
+        return IF.GUESS_FRAME_OBJECTS['label']['text']
 
     def ask_questions(self, game: GW.GuessWho) -> str:
         """Asks a question based on which question the player selected."""
-        return game_settings['question'] + '?'
+        return GAME_SETTINGS['question'] + '?'
 
 
 def make_a_guess() -> None:
-    """Makes the current guess for the HumanPlayer"""
+    """Makes the current guess for the HumanPlayer.
+    Clears chat box.
+    Runs the guess through the game.
+    Possible tie covered.
+    Disables all buttons.
+    """
     IF.clear_chat()
-    p1 = game_settings['players'][0]
-    q1 = p1.make_guesses(game_settings['game'])
-    # print(game_settings['players'][1].spy, q1)
-    if game_settings['players'][1].spy == q1:
-        g2 = game_settings['players'][1].make_guesses(game_settings['game'])
+    p1 = GAME_SETTINGS['players'][0]
+    q1 = p1.make_guesses(GAME_SETTINGS['game'])
+
+    g2 = GAME_SETTINGS['players'][1].make_guesses(GAME_SETTINGS['game'])
+    if GAME_SETTINGS['players'][1].spy == q1:
         IF.insert_conversation('You are correct!\n\nComputer made \nthe final guess:\n' + g2 + '\n')
-        if g2 == game_settings['players'][0].spy:
+
+        if g2 == GAME_SETTINGS['players'][0].spy:
             IF.insert_conversation('The Computer \nguessed correctly!\nIt is a tie\n')
+
         else:
             IF.insert_conversation('The Computer \nguessed incorrectly!\nYou win!\n')
+
+    else:
+        IF.insert_conversation('You are incorrect!\n\nComputer made \nthe final guess:\n' + g2 + '\n')
+        if g2 == GAME_SETTINGS['players'][0].spy:
+            IF.insert_conversation('The Computer \nguessed correctly!\nYou lose!\n')
+
+        else:
+            IF.insert_conversation('The Computer \nguessed incorrectly!\nIt is a tie!\n')
+
     disable_all()
 
 
 def make_a_final_guess() -> None:
-    """Makes the current final guess for the HumanPlayer"""
-    p1 = game_settings['players'][0]
-    q1 = p1.make_guesses(game_settings['game'])
-    # print(game_settings['players'][1].spy, q1)
-    if game_settings['players'][1].spy == q1:
+    """Makes the current final guess for the HumanPlayer. This is the guess where the computer has already guessed."""
+    p1 = GAME_SETTINGS['players'][0]
+    q1 = p1.make_guesses(GAME_SETTINGS['game'])
+
+    if GAME_SETTINGS['players'][1].spy == q1:
         IF.insert_conversation('You are correct!\nIt is a tie!')
     else:
         IF.insert_conversation('You lost!\n')
-    disable_all()
+
     IF.insert_conversation('''Let's play again!\nClick menu\nto reset''')
     disable_all()
 
@@ -116,202 +133,213 @@ def make_a_final_guess() -> None:
 def make_a_question() -> None:
     """Makes a question for the HumanPlayer"""
     IF.clear_chat()
-    p1 = game_settings['players'][0]
-    q1 = p1.ask_questions(game_settings['game'])
+    p1 = GAME_SETTINGS['players'][0]
+    q1 = p1.ask_questions(GAME_SETTINGS['game'])
     IF.insert_conversation('You asked if the \ncomputer has:\n'
-                           + IF.convert_feature_to_text(game_settings['question'])
+                           + IF.convert_feature_to_text(GAME_SETTINGS['question'])
                            + '\n')
-    a1 = game_settings['game'].return_answer(q1, 2)  # Y or N
+
+    a1 = GAME_SETTINGS['game'].return_answer(q1, 2)  # Y or N
+
     if a1 == 'Y':
         IF.insert_conversation('Yes!\nMy spy has the\nfeature:\n'
-                               + IF.convert_feature_to_text(game_settings['question']) + '\n')
-        for face in IF.game_features['faces']:
-            if game_settings['question'] not in face.chars:
+                               + IF.convert_feature_to_text(GAME_SETTINGS['question']) + '\n')
+
+        for face in IF.GAME_FEATURES['faces']:
+            if GAME_SETTINGS['question'] not in face.chars:
                 face.up = False
                 face.cover()
+
     else:
         IF.insert_conversation('No!\nMy spy does not have\nthe feature:\n'
-                               + IF.convert_feature_to_text(game_settings['question']) + '\n')
-        for face in IF.game_features['faces']:
-            if game_settings['question'] in face.chars:
+                               + IF.convert_feature_to_text(GAME_SETTINGS['question']) + '\n')
+
+        for face in IF.GAME_FEATURES['faces']:
+            if GAME_SETTINGS['question'] in face.chars:
                 face.up = False
                 face.cover()
+
     p1.eliminate_candidates(q1, a1)
     computer_make_a_question()
 
 
 def disable_all() -> None:
-    """Disables all buttons on the main board."""
-    for b in IF.question_frame_objects['buttons']:
+    """Disables all buttons on the main board. (except menu and exit)"""
+    for b in IF.QUESTION_FRAME_OBJECTS['buttons']:
         b.config(state=DISABLED)
-    IF.question_frame_objects['questionbutton'].config(state=DISABLED)
-    IF.question_frame_objects['clearbutton'].config(state=DISABLED)
+    IF.QUESTION_FRAME_OBJECTS['questionbutton'].config(state=DISABLED)
+    IF.QUESTION_FRAME_OBJECTS['clearbutton'].config(state=DISABLED)
 
-    IF.guess_frame_objects['guessbutton'].config(state=DISABLED)
-    IF.guess_frame_objects['clearbutton'].config(state=DISABLED)
+    IF.GUESS_FRAME_OBJECTS['guessbutton'].config(state=DISABLED)
+    IF.GUESS_FRAME_OBJECTS['clearbutton'].config(state=DISABLED)
 
 
 def enable_all() -> None:
     """Disables all buttons on the main board."""
-    for b in IF.question_frame_objects['buttons']:
+    for b in IF.QUESTION_FRAME_OBJECTS['buttons']:
         b.config(state=NORMAL)
-    IF.question_frame_objects['questionbutton'].config(state=DISABLED)
-    IF.question_frame_objects['clearbutton'].config(state=NORMAL)
+    IF.QUESTION_FRAME_OBJECTS['questionbutton'].config(state=DISABLED)
+    IF.QUESTION_FRAME_OBJECTS['clearbutton'].config(state=NORMAL)
 
-    IF.guess_frame_objects['guessbutton'].config(state=DISABLED)
-    IF.guess_frame_objects['clearbutton'].config(state=NORMAL)
-
-
-def fake_think() -> None:
-    """Fakes thinking like a computer"""
-    game_settings['window'].after(6000, fake_think)
+    IF.GUESS_FRAME_OBJECTS['guessbutton'].config(state=DISABLED)
+    IF.GUESS_FRAME_OBJECTS['clearbutton'].config(state=NORMAL)
 
 
 def computer_make_a_guess() -> None:
     """Makes the computer make its guess and gives the player an option to make their guess."""
-    g2 = game_settings['players'][1].make_guesses(game_settings['game'])
+    g2 = GAME_SETTINGS['players'][1].make_guesses(GAME_SETTINGS['game'])
+
     IF.insert_conversation('You are correct!\nComputer made \nthe final guess:\n' + g2 + '\n')
     IF.insert_conversation('The Computer \nguessed correctly!\nIt is your turn\nto tie it up!')
+
     disable_all()
-    IF.guess_frame_objects['clearbutton'].config(state=NORMAL)
-    IF.guess_frame_objects['guessbutton'].config(command=make_a_final_guess)
+
+    IF.GUESS_FRAME_OBJECTS['clearbutton'].config(state=NORMAL)
+    IF.GUESS_FRAME_OBJECTS['guessbutton'].config(command=make_a_final_guess)
     IF.clear_selection()
 
 
 def computer_make_a_question() -> None:
-    """Makes the computer make its question move"""
+    """Makes the computer make its question move."""
     disable_all()
-    # IF.conversation_frame_objects['bar'].start()
-    p2 = game_settings['players'][1]
+
+    p2 = GAME_SETTINGS['players'][1]
+
     if len(p2.candidates) == 1:
         computer_make_a_guess()
     else:
-        q2 = p2.ask_questions(game_settings['game'])
-        a2 = game_settings['game'].return_answer(q2, 1)
+        q2 = p2.ask_questions(GAME_SETTINGS['game'])
+        a2 = GAME_SETTINGS['game'].return_answer(q2, 1)
         p2.eliminate_candidates(q2, a2)
-        fake_think()
         IF.insert_conversation('The computer has \nasked if you have:\n' + IF.convert_feature_to_text(q2[0]) + '\n')
-        fake_think()
+
         if a2 == 'Y':
             IF.insert_conversation('Your spy has this\nattribute!\n')
         else:
             IF.insert_conversation('Your spy does not have this attribute!\n')
         enable_all()
-        # IF.conversation_frame_objects['bar'].stop()
         IF.clear_selection()
 
 
 def select_question(button: Button) -> None:
-    """Selects a question and stores it"""
-    global game_settings
-    game_settings['question'] = IF.question_to_feature(button['text'])
-    for b in IF.question_frame_objects['buttons']:
+    """Selects a question and stores it from the specific button."""
+    global GAME_SETTINGS
+
+    GAME_SETTINGS['question'] = IF.question_to_feature(button['text'])
+
+    for b in IF.QUESTION_FRAME_OBJECTS['buttons']:
+
         if b['text'] == button['text']:
             b.config(state=DISABLED)
+
         else:
             b.config(state=NORMAL)
-    IF.question_frame_objects['label'].config(text=button['text'])
-    IF.question_frame_objects['questionbutton'].config(state=NORMAL)
 
-
-def determine_if_enabled(button: Button) -> None:
-    """Determines whether or no the question button should be enabled."""
-    # print(game_settings['players'][0].questions)
-    question = IF.question_to_feature(button['text']) + '?'
-    # print(question, question in game_settings['players'][0].questions)
-    if question in game_settings['players'][0].questions and button['text'] != game_settings['question']:
-        button.config(state=NORMAL)
-    else:
-        button.config(state=DISABLED)
+    IF.QUESTION_FRAME_OBJECTS['label'].config(text=button['text'])
+    IF.QUESTION_FRAME_OBJECTS['questionbutton'].config(state=NORMAL)
 
 
 def text_to_player(text: str) -> GW.Player:
-    """Converts the text to the respective player and initializes the object, then returns it."""
+    """Converts the text to the respective player and initializes the object, then returns it.
+    Necessary for selecting a difficulty."""
     if text == 'GreedyPlayer':
-        return AI.GreedyPlayer(game_settings['candidates'].copy(), game_settings['questions'].copy())
-    if text == 'RandomPlayer':
-        return AI.RandomPlayer(game_settings['candidates'].copy(), game_settings['questions'].copy())
-    if text == 'PoorPlayer':
-        return AI.PoorPlayer(game_settings['candidates'].copy(), game_settings['questions'].copy())
+        return AI.GreedyPlayer(GAME_SETTINGS['candidates'].copy(), GAME_SETTINGS['questions'].copy())
+    elif text == 'RandomPlayer':
+        return AI.RandomPlayer(GAME_SETTINGS['candidates'].copy(), GAME_SETTINGS['questions'].copy())
+    else:
+        return AI.PoorPlayer(GAME_SETTINGS['candidates'].copy(), GAME_SETTINGS['questions'].copy())
 
 
 def update_question_buttons() -> None:
-    """Updates all the question buttons in the button frame."""
-    IF.game_features['menu'].config(command=new_game)
-    for button in IF.question_frame_objects['buttons']:
+    """Updates all the question buttons in the button frame to be able to select a question or make a guess."""
+    IF.GAME_FEATURES['menu'].config(command=new_game)
+
+    for button in IF.QUESTION_FRAME_OBJECTS['buttons']:
         q = IF.question_to_feature(button['text']) + '?'
-        if q in game_settings['players'][0].questions:
+
+        if q in GAME_SETTINGS['players'][0].questions:
             button.config(state=NORMAL, command=lambda b=button: select_question(b))
-    IF.guess_frame_objects['guessbutton'].config(command=make_a_guess)
-    IF.question_frame_objects['questionbutton'].config(command=make_a_question)
+
+    IF.GUESS_FRAME_OBJECTS['guessbutton'].config(command=make_a_guess)
+    IF.QUESTION_FRAME_OBJECTS['questionbutton'].config(command=make_a_question)
 
 
 def select_player(difficulty: str) -> None:
     """Selects the player that will play (AI)"""
-    global game_settings
+    global GAME_SETTINGS
 
-    # print(player1.spy)
-    player2 = text_to_player(difficulty)
-    player2.select_spy()
-    print(player2.spy)
-    game_settings['players'].append(player2)
+    player2 = text_to_player(difficulty)  # Finds difficulty
+    player2.select_spy()  # Randomly chooses spy
+
+    # print(player2.spy)  # Enable to guess eaiser
+
+    GAME_SETTINGS['players'].append(player2)
     IF.clear_frame()
     IF.summon_main_board()
-    spy = IF.name_to_face(game_settings['players'][0].spy)
-    spy.canvas = IF.game_features['spycanvas']
+
+    spy = IF.name_to_face(GAME_SETTINGS['players'][0].spy)
+    spy.canvas = IF.GAME_FEATURES['spycanvas']
+
+    # Necessary for face to grid properly
     spy.row = 1
     spy.col = 0
     spy.draw_face()
+
     update_question_buttons()
-    game_settings['game'] = GW.GuessWho(game_settings['players'], game_settings['candidates'])
+    GAME_SETTINGS['game'] = GW.GuessWho(GAME_SETTINGS['players'], GAME_SETTINGS['candidates'])
 
 
 def run_window() -> None:
-    """Runs the main GUI"""
+    """Runs the main GUI. Give player buttons the select player option in the main menu."""
     IF.summon_main_menu()
-    for button in IF.game_features['pb']:
+    for button in IF.GAME_FEATURES['pb']:
         button.config(command=lambda p=button['text']: select_player(p))
 
 
 def new_game() -> None:
-    """Starts a new game of GuessWho"""
-    game_settings['candidates'] = GW.create_candidates('data/questions.csv', 24)
-    candidates1 = game_settings['candidates'].copy()
-    game_settings['questions'] = GW.generate_all_possible_questions('data/questions.csv')
+    """Starts a new game of GuessWho. Frehs start and then run the main window of the game."""
+    GAME_SETTINGS['candidates'] = GW.create_candidates('data/questions.csv', 24)  # 24 people NECESSARY.
+    candidates1 = GAME_SETTINGS['candidates'].copy()
+    GAME_SETTINGS['questions'] = GW.generate_all_possible_questions('data/questions.csv')
 
-    questions1 = game_settings['questions'].copy()
+    questions1 = GAME_SETTINGS['questions'].copy()
     player1 = HumanPlayer(candidates1, questions1)
     player1.select_spy()
-    game_settings['players'] = [player1]
+    GAME_SETTINGS['players'] = [player1]
     run_window()
 
 
 def initiate() -> None:
-    """Runs the main code for the game."""
+    """Runs the main code for the game.
+    Initiate the main frame and winow."""
     window = Tk()
-    game_settings['window'] = window
+    GAME_SETTINGS['window'] = window
 
     frame = Frame(window, width=900, height=600)
     frame.pack(side="top", expand=True, fill="both")
     frame.pack_propagate(False)
     IF.set_frame(frame)
+
     new_game()
     run_window()
     window.mainloop()
 
 
+if __name__ == "__main__":
+    initiate()
+
 if __name__ == '__main__':
-#     candidates = guess_who.create_candidates('data/questions.csv', 12)  # Define data to initialize two players
-#     candidates1 = candidates.copy()
-#     questions = guess_who.generate_all_possible_questions('data/questions.csv')
+    # candidates = guess_who.create_candidates('data/questions.csv', 12)  # Define data to initialize two players
+    # candidates1 = candidates.copy()
+    # questions = guess_who.generate_all_possible_questions('data/questions.csv')
 
     # Sample call to run GuessWho 100 times between GreedyPlayer and PoorPlayer.
     # You may change the first parameter in run_games to determine how many games you would like to run.
     # You may change the last two parameters False, to not print out the winning probability of
     # each player(last parameter), or to not plot the game results(second last parameter).
-#     player1 = GreedyPlayer(candidates, questions)
-#     player2 = PoorPlayer(candidates1, questions)
-#     run_games(100, [player1, player2], 12, 'data/questions.csv', True, True)
+    # player1 = GreedyPlayer(candidates, questions)
+    # player2 = PoorPlayer(candidates1, questions)
+    # run_games(100, [player1, player2], 12, 'data/questions.csv', True, True)
     # You can see GreedyPlayer has obviously better performance.
 
     # Below is a Sample call to run GuessWho 100 times with same setting, but between GreedyPlayer
@@ -321,9 +349,22 @@ if __name__ == '__main__':
     # run_games(100, [player1, player2], 12, 'data/questions.csv', True, True)
 
     # You should notice a more nuanced difference between the performance of RandomPlayer and GreedyPlayer
-    # and the performance of PoorPlayer and GreedyPlayer. Sometimes GreedyPlayer and RandoPlayer may even have the same 
+    # and the performance of PoorPlayer and GreedyPlayer. Sometimes GreedyPlayer and RandoPlayer may even have the same
     # winning probability!
-    
-    # Starts the main GUI
-    initiate()
-    
+
+    import python_ta
+
+    python_ta.check_all(config={
+        'max-line-length': 120,
+        'extra-imports': ['tkinter', 'guess_who', 'features', 'artificial_intelligence', 'csv', 'interface'],
+        'disable': ['forbidden-top-level-code',
+                    'wildcard-import',
+                    'too-many-branches',
+                    'forbidden-global-variables',
+                    'unused-argument',
+                    'too-many-arguments',
+                    'too-many-locals',
+                    'unused-import'
+                    ],
+        'allowed-io': []
+    })
